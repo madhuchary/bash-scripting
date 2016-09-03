267d244658d0f48c63aadc214dc62835b51a2eec
madhu sudhan <madhuchary80@gmail.com>
2016-09-03 06:07:18 -0700

    jenkins

--- first.sh
+++ first.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-#version 1.1
-# The above line is called hash bang and tells the below scripting language 
-# This script checks user age and tells if he is allowed for voting or not allowed
-echo "Please enter your age"
-read age
-if [ $age -ge 18 ]
-then
-echo "allowed"
-else
-echo "not allowed"
-fi
+echo "hello jenkins"
